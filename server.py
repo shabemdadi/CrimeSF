fa5230b69c7f4c1751c742867874dc64c4fbada6
Shabnam Emdadi <shab.emdadi@gmail.com>
2015-05-13 22:54:38 -0700

    Added in crime incident markers

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from jinja2 import StrictUndefined
-from flask import Flask, render_template, redirect, request, flash, session
+from flask import Flask, render_template, redirect, request, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from model import Crime_Stat,connect_to_db, db
 import json
@@ -23,16 +23,17 @@ def index():
 def get_crime_stats():
 
     crime_stats = Crime_Stat.query.limit(10).all()
-    geoJSON_compiled = {}
+    crime_dict = {}
     
-    for crime in crime_stats:
-        geoJSON_compiled[crime.incident_id] = {}
-        geoJSON_compiled[crime.incident_id]['lat'] = str(decimal.Decimal(crime.x_cord))
-        geoJSON_compiled[crime.incident_id]['long'] = str(decimal.Decimal(crime.y_cord))
-        geoJSON_compiled[crime.incident_id]['id'] = crime.incident_id
-        # geoJSON_compiled[crime.incident_id]['address'] = crime.address
-    
-    return jsonify(geoJSON_compiled)
+    for crime in crime_stats:           # need to add in address
+        crime_dict[crime.incident_id] = {}
+        crime_dict[crime.incident_id]['lat'] = str(decimal.Decimal(crime.x_cord))
+        crime_dict[crime.incident_id]['long'] = str(decimal.Decimal(crime.y_cord))
+        crime_dict[crime.incident_id]['id'] = crime.incident_id
+        
+    print jsonify(crime_dict)
+    return jsonify(crime_dict)
+                
     
 @app.route('/users')
 def get_user_profile():

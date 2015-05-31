a10924a0aba555bbbbda784ff3ae76d2b00064a2
Shabnam Emdadi <shab.emdadi@gmail.com>
2015-05-31 13:14:16 -0700

    finished buffer zone functionality, added in filters to journey page

--- static/JS/journey.js
+++ static/JS/journey.js
@@ -22,7 +22,6 @@ var directionsInstructionsControl = L.mapbox.directions.instructionsControl('ins
 var feature_layer = L.mapbox.featureLayer(); //define the feature layer
 
 function addMarkerLayer(data) { //this will add markers to the map
-  startLoading();         // loading screen will start
   feature_layer = L.mapbox.featureLayer(data).addTo(map); //GeoJSON feature objects will be added to the feature_layer, which will be added to the map
   feature_layer.on('click', function(e) {                 //map will zoom into a marker if a user clicks on it
       map.panTo(e.layer.getLatLng());
@@ -40,23 +39,64 @@ function addMarkerLayer(data) { //this will add markers to the map
           '<\/p>';
       layer.bindPopup(content);
     });
-  finishedLoading();
 };
 
-$.getJSON('/get_markers', { start_date: [], end_date: [] } ).done(function(data){ //this will load when the user goes to the points of interest page, it will show crimes in the default date range period
-    console.log("markers is running");
-    addMarkerLayer(data);
-  });
+function addFilters() {     //this function will add the filter, and create an event listener that will update the marker points when a user checks or unchecks the filter checkboxes
+// Wait until the marker layer is loaded in order to build a list of possible types. If you are doing this with another featureLayer, you should change map.featureLayer to the variable you have assigned to your featureLayer
+    // feature_layer.on('ready', function() {
+// Collect the types of symbols in this layer. you can also just hardcode an array of types if you know what you want to filter on, like var types = ['foo', 'bar'];
+  var typesObj = {}, types = [];
+  var features = feature_layer.getGeoJSON().features; //get the features objects from the feature layer
+  for (var i = 0; i < features.length; i++) {         //iterate over feature objects, creating a dictionary where the key is the description property of each marker, and the value is true
+    typesObj[features[i].properties['title']] = true;
+    }
 
-directions.on("load",function(){
+  for (var k in typesObj) types.push(k);            //create a list of the descriptions
+
+  var checkboxes = [];
+  // Create a filter interface.
+  for (var i = 0; i < types.length; i++) {        //iterate over the descriptions and create DOM div>input objects for each one as a checkbox, and with the id as the description
+    // Create an an input checkbox and label inside.
+    var item = filters.appendChild(document.createElement('div'));
+    var checkbox = item.appendChild(document.createElement('input'));
+    var label = item.appendChild(document.createElement('label'));
+    checkbox.type = 'checkbox';
+    checkbox.id = types[i];
+    checkbox.checked = true;
+    // create a label to the right of the checkbox with explanatory text
+    label.innerHTML = types[i]; 
+    label.setAttribute('for', types[i]);
+    // Whenever a person clicks on this checkbox, call the update().
+    checkbox.addEventListener('change', update);
+    checkboxes.push(checkbox);
+  };
+
+  // This function is called whenever someone clicks on a checkbox and changes the selection of markers to be displayed.
+  function update() {
+    console.log("update has been called");
+    var enabled = {};
+    // Run through each checkbox and record whether it is checked. If it is, add it to the object of types to display, otherwise do not.
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked) enabled[checkboxes[i].id] = true;
+    }
+    feature_layer.setFilter(function(feature) {
+      // If this symbol is in the list, return true. if not, return false. The 'in' operator in javascript does exactly that: given a string or number, it says if that is in 
+      // a object.
+      return (feature.properties['title'] in enabled);
+    });
+  };
+};
+
+function createBuffer (){
 	console.log(directions.directions.routes[0].geometry.coordinates);
 	console.log(directions.directions.routes[0]);
 	// var route_line = turf.linestring([directions.directions.routes[0].geometry.coordinates], { name: 'route_line' } );
 	// console.log(route_line);
 	var route_line = directions.directions.routes[0];
-	debugger;
 	var unit = 'miles';
-	var buffered = turf.buffer(route_line, 0.1, unit);
+	var buffer_dist = $("#buffer_choice").val();
+	console.log(buffer_dist);
+	var buffered = turf.buffer(route_line, buffer_dist, unit);
 	console.log(buffered);
 	var result = turf.featurecollection([buffered, route_line]);
 	console.log(result);
@@ -64,6 +104,35 @@ directions.on("load",function(){
 	console.log(markers);
 	var markersWithin = turf.within(markers, buffered);
 	console.log(markersWithin);
+	feature_layer.setGeoJSON([])
+	addMarkerLayer(markersWithin);
+};
+
+$.getJSON('/get_markers', { start_date: [], end_date: [] } ).done(function(data){ //this will load when the user goes to the points of interest page, it will show crimes in the default date range period
+    console.log("markers is running");
+    startLoading();
+    addMarkerLayer(data);
+    addFilters();
+    finishedLoading();
+  });
+
+directions.on("load",function(){
+	createBuffer();
+	$("#filters").empty(); //empty the filters element so that a new filter list can be created
+    addFilters();
 });
 
-$("option[name='buffer']").val();
+$("#buffer_choice").change(function(){
+	startLoading();
+	console.log("buffer changed");
+	feature_layer.setGeoJSON([]);
+	$.getJSON('/get_markers', { start_date: [], end_date: [] } ).done(function(data){ //this will load when the user goes to the points of interest page, it will show crimes in the default date range period
+    console.log("markers is running");
+    addMarkerLayer(data);
+    debugger;
+    createBuffer();
+    $("#filters").empty(); //empty the filters element so that a new filter list can be created
+    addFilters();
+  	});
+  	finishedLoading();
+});

54176476e0471fc37cc5f65c85207c75513ff275
Shabnam Emdadi <shab.emdadi@gmail.com>
2015-06-10 21:28:45 -0700

    cleaned up

--- server.py
+++ server.py
@@ -51,7 +51,7 @@ def get_marker_points():
     else:                               # user has not entered in a date, use a default period
         
         end_date = datetime.now()                    
-        start_date = end_date - timedelta(days=15)
+        start_date = end_date - timedelta(days=16)
 
         print start_date
 
@@ -84,7 +84,7 @@ def get_heat_points():
     else:                               # user has not entered in a date, use a default period
         
         end_date = datetime.now()                    
-        start_date = end_date - timedelta(days=15)
+        start_date = end_date - timedelta(days=16)
 
         print start_date
 
